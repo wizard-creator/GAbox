5cd63da2563f3bd7584cbdaa557bee7007fce3cf
chunhui_cpu1 <1772662323@qq.com>
2024-05-30 22:04:33 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@ File Name      :  setup.py
+@ Time           :  2024/05/30 22:03:26
+@ Author         :  Chunhui Zhang
+@ Version        :  0.1
+@ Contact        :  1772662323@qq.com
+@ Description    :  description of the code
+@ History        :  0.1(2024/05/30) - setup for GAbox(zhangchunhui)
+'''
+
+
+from setuptools import setup
+
+setup(
+    name='GAbox',
+    version='0.1',
+    author='chunhuiZhang',
+    author_email='你的邮件',
+    description='Zhang GA box',
+    license='MIT',
+    url='git@github.com:wizard-creator/GAbox.git',
+)
